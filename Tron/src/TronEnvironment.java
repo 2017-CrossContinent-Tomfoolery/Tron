73b0345e6789fde3e5e28daf2a5b657124452cd4
Benjamin Wong <benjamin.wong@brentwood.ca>
2017-05-07 21:33:52 -0700

    Drawing a grid without the library

--- Tron/src/TronEnvironment.java
+++
@@ -1,9 +0,0 @@
-
-public class TronEnvironment {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to TRON");
-	}
-
-}
---
+++ Tron/src/game/Game.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+//import javax.swing.WindowConstants;
+/**
+ *
+ * @author benjaminwong
+ */
+@SuppressWarnings("serial")
+public class Game extends JPanel {
+    int x = 0;
+    int y = 0;
+    
+    Point topLeftLocation = new Point(10, 10);
+    int rows = 5; int cols = 10; int dimension = 10;
+    
+    
+    private void moveBall() {
+        x = x + 1;
+        y = y + 1;
+    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        //<editor-fold defaultstate="collapsed" desc="Anti-Alias">
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        //</editor-fold>
+        
+        for (int row = 0; row < rows; row++){
+            for (int col = 0; col < cols; col++){
+                g2d.drawRect(topLeftLocation.x + col * dimension, topLeftLocation.y + row * dimension, 
+                        dimension, dimension);
+            }
+        }
+        
+        //g2d.fillOval(x, y, 30, 30);
+    }
+    
+    
+
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Sample Frame");
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true) {
+            //game.moveBall();
+            game.repaint();
+            Thread.sleep(10);
+        }
+    }
+}
